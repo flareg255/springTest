e12186476f9a6d19916b96bf51f8e47957be3d8b
flareg255 <flare.g255@gmail.com>
2018-12-02 10:29:54 +0900

    date createuser

--- src/main/java/com/example/bbs/Bbs.java
+++ src/main/java/com/example/bbs/Bbs.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Demo8Application {
+public class Bbs {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Demo8Application.class, args);
+		SpringApplication.run(Bbs.class, args);
 	}
 }
--- src/main/java/com/example/bbs/ServletInitializer.java
+++ src/main/java/com/example/bbs/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(Demo8Application.class);
+		return application.sources(Bbs.class);
 	}
 
 }
